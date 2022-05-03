3f75c3315800c7cd0864a8c5d67ba122a316311d
Nero Li <1109680304lym@gmail.com>
2022-05-03 14:05:04 -0700

    in-class

---
+++ main.asm
@@ -0,0 +1,24 @@
+; who: Nero Li, yli342
+; what: <the function of this program>
+; why: <the name of the lab>
+; when: 2022-MM-DD
+
+section     .text
+
+global      _start
+extern      print_string
+extern      get_input
+
+_start:
+
+
+
+exit:  
+    mov     ebx, 0      ; return 0 status on exit - 'No Errors'
+    mov     eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
+    int     80h
+
+section     .bss
+
+section     .data
+
