8ca2ec98bffe97bab4a576390decaa9d36ebd5d0
Nero Li <1109680304lym@gmail.com>
2022-05-12 15:35:19 -0700

    working on Lab 9a

---
+++ topic_9a/main.asm
@@ -0,0 +1,28 @@
+; who: Nero Li, yli342
+; what: A program that tests each procedure in string_lib.asm
+; why: Lab 9a: String Procedures
+; when: 2022-05-18
+
+section     .text
+
+global      _start
+
+extern      size_of
+extern      print_string
+extern      string_copy
+extern      to_lower
+extern 	    to_upper
+
+_start:
+    push    array
+    call    print_string
+
+exit:  
+    mov     ebx, 0      ; return 0 status on exit - 'No Errors'
+    mov     eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
+    int     80h
+
+section     .bss
+
+section     .data
+    array:  db  "Hello world!", 0Ah, 0
---
+++ topic_9a/string_lib.asm
@@ -0,0 +1,128 @@
+; who: Nero Li, yli342
+; what: File with all of the string procedures implemented
+; why: Lab 9a: String Procedures
+; when: 2022-05-18
+
+section     .text
+
+global      size_of
+global      print_string
+global      string_copy
+global      to_lower
+global 	    to_upper
+
+;----------------------------------------------------------------------------------------
+size_of:
+; 
+; This displays the size of a string that has been null-terminated.
+; Receives: arg1 = the address of a null-terminated string
+; Returns: 	EAX = the size of the string (not including the null terminator)
+; Requires:	Nothing
+; Note:     Nothing
+;----------------------------------------------------------------------------------------
+    push    ebp                 ; preserve caller's base pointer
+    mov     ebp, esp            ; set base pointer for frame
+    push    esi                 ; preserve esi
+
+    mov     esi, [ebp + 8]      ; move arg1 into esi (address of the string)
+    mov     eax, 0              ; size number start at zero
+
+    .loop:
+    cmp     [esi], 0            ; check if esi is at the null terminator
+    je      .exit
+    inc     eax                 ; one character found, ++size
+    inc     esi                 ; check the next character
+    jmp     .loop
+
+    .end:
+    pop     esi                 ; restore esi
+    pop     ebp                 ; restore caller's base pointer
+    ret
+; End size_of ---------------------------------------------------------------------------
+
+;----------------------------------------------------------------------------------------
+print_string:
+; 
+; This displays a string that has been null-terminated.
+; Receives: arg1 = the address of a null-terminated string
+; Returns: 	Nothing
+; Requires:	Nothing
+; Note:     Nothing
+;----------------------------------------------------------------------------------------
+    push    ebp                 ; preserve caller's base pointer
+    mov     ebp, esp            ; set base pointer for frame
+    push    eax                 ; preserve eax
+    push    ebx                 ; preserve ebx
+    push    ecx                 ; preserve ecx
+    push    edx                 ; preserve edx
+
+    mov     ecx, [ebp + 8]      ; move arg1 into ecx (address of the string)
+    push    ecx                 ; push ecx into stack for procedure call
+    call    size_of             ; get size of the string
+    mov     edx, eax            ; move size of the string into edx
+    mov     eax, 4              ; set stream as stdout
+    mov     ebx, 1              ; set write code
+    int     80h                 ; syscall    
+
+    pop     edx                 ; restore edx
+    pop     ecx                 ; restore ecx
+    pop     ebx                 ; restore ebx
+    pop     eax                 ; restore eax
+    pop     ebp                 ; restore caller's base pointer
+    ret
+; End print_string ----------------------------------------------------------------------
+
+;----------------------------------------------------------------------------------------
+string_copy:
+; 
+; Copy a null terminated string from one array into another.
+; Receives: arg1 = the address of the source string
+;           arg2 = the address of the destination string
+; Returns: 	Nothing
+; Requires:	Nothing
+; Note:     Nothing
+;----------------------------------------------------------------------------------------
+    push    ebp                 ; preserve caller's base pointer
+    mov     ebp, esp            ; set base pointer for frame
+
+    
+
+    pop     ebp                 ; restore caller's base pointer
+    ret
+; End string_copy -----------------------------------------------------------------------
+
+;----------------------------------------------------------------------------------------
+to_lower:
+; 
+; Scan the string for uppercase alphabet characters and convert them to lowercase.
+; Receives: arg1 = the address of a null-terminated string
+; Returns: 	Nothing
+; Requires:	Nothing
+; Note:     Nothing
+;----------------------------------------------------------------------------------------
+    push    ebp                 ; preserve caller's base pointer
+    mov     ebp, esp            ; set base pointer for frame
+
+    
+
+    pop     ebp                 ; restore caller's base pointer
+    ret
+; End to_lower --------------------------------------------------------------------------
+
+;----------------------------------------------------------------------------------------
+to_upper:
+; 
+; Set the random number seed
+; Receives: arg1 = the address of a null-terminated string
+; Returns: 	Nothing
+; Requires:	Nothing
+; Note:     Nothing
+;----------------------------------------------------------------------------------------
+    push    ebp                 ; preserve caller's base pointer
+    mov     ebp, esp            ; set base pointer for frame
+
+    
+
+    pop     ebp                 ; restore caller's base pointer
+    ret
+; End to_upper --------------------------------------------------------------------------
