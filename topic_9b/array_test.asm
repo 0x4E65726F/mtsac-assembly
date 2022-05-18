7499acbf20c5c1c42fe8fcd9a4dccdb2ea66859e
Nero Li <1109680304lym@gmail.com>
2022-05-18 14:29:27 -0700

    Finish Lab 9b

--- topic_9b/array_test.asm
+++ topic_9b/array_test.asm
@@ -17,7 +17,25 @@ _start:
 
     pop     eax
     pop     eax
-    push    dword [search]
+    push    dword [search_3]
+    push    array_sz
+    push    array
+    call    bin_search_d
+    mov     ebx, [eax]
+
+    pop     eax
+    pop     eax
+    pop     eax
+    push    dword [search_5]
+    push    array_sz
+    push    array
+    call    bin_search_d
+    mov     ebx, [eax]
+
+    pop     eax
+    pop     eax
+    pop     eax
+    push    dword [search_7]
     push    array_sz
     push    array
     call    bin_search_d
@@ -33,4 +51,6 @@ section     .bss
 section     .data
     array:      dd      1, 3, 2, 5, 4, 8, 9, 6, 7, 0
     array_sz:   equ     $ - array
-    search:     dd      7
+    search_3:   dd      3
+    search_5:   dd      5
+    search_7:   dd      7
