69f0773beae1ba3ef2060375ee1da3f02b5cebb7
Nero Li <1109680304lym@gmail.com>
2022-05-17 20:35:42 -0700

    9b

--- topic_9b/array_lib.asm
+++ topic_9b/array_lib.asm
@@ -120,7 +120,7 @@ bin_search_d:
 
     .skip:
     cmp     [esi + ebx], edx
-    jl      .greater                ; if array[mid] > term then go to greater
+    jg      .greater                ; if array[mid] > term then go to greater
     lea     edi, [esi + ebx - 4]    ; else array[mid] < term so last = mid - 1
     jmp     .while                  ; loop
 
