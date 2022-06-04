eba009e11b3520b55e3a6d770dfcefa3fcc4a78a
Nero Li <1109680304lym@gmail.com>
2022-06-03 21:09:30 -0700

    checking final

--- final/main.asm
+++ final/main.asm
@@ -96,10 +96,8 @@ _start:
     call    print_string
     add     esp, 8
 
-exit:  
-    mov     ebx, 0      ; return 0 status on exit - 'No Errors'
-    mov     eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
-    int     80h
+    push    dword 0
+    call    exit
 
 section     .bss     
     buffer:     resb    1024
