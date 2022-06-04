fff38ab0608e463e8a337bbf57832240b8f30f5e
Nero Li <1109680304lym@gmail.com>
2022-06-03 21:07:53 -0700

    checking final library

--- final/main.asm
+++ final/main.asm
@@ -5,6 +5,34 @@ section     .text
 global      _start
 
 _start:
+    call    current_time
+    call    srand
+    call    rand
+    push    buffer_sz
+    push    buffer
+    push    eax
+    call    itoa
+    add     esp, 12
+
+    push    eax
+    push    buffer
+    call    print_string
+    add     esp, 8
+    
+    call    current_time
+    call    srand
+    call    rand
+    push    buffer_sz
+    push    buffer
+    push    eax
+    call    itoa
+    add     esp, 12
+
+    push    eax
+    push    buffer
+    call    print_string
+    add     esp, 8
+
     push    array_sz
     push    array
     call    bubble_sort_d
@@ -17,6 +45,17 @@ _start:
     call    bin_search_d
     mov     ebx, [eax]
 
+    push    buffer_sz
+    push    buffer
+    push    ebx
+    call    itoa
+    add     esp, 12
+
+    push    eax
+    push    buffer
+    call    print_string
+    add     esp, 8
+
     pop     eax
     pop     eax
     pop     eax
@@ -25,6 +64,17 @@ _start:
     push    array
     call    bin_search_d
     mov     ebx, [eax]
+
+    push    buffer_sz
+    push    buffer
+    push    ebx
+    call    itoa
+    add     esp, 12
+
+    push    eax
+    push    buffer
+    call    print_string
+    add     esp, 8
 
     pop     eax
     pop     eax
@@ -35,16 +85,29 @@ _start:
     call    bin_search_d
     mov     ebx, [eax]
 
+    push    buffer_sz
+    push    buffer
+    push    ebx
+    call    itoa
+    add     esp, 12
+
+    push    eax
+    push    buffer
+    call    print_string
+    add     esp, 8
+
 exit:  
     mov     ebx, 0      ; return 0 status on exit - 'No Errors'
     mov     eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
     int     80h
 
 section     .bss     
+    buffer:     resb    1024
+    buffer_sz:  equ     $ - buffer
 
 section     .data
     array:      dd      1, 3, 2, 5, 4, 8, 9, 6, 7, 0
     array_sz:   equ     $ - array
     search_3:   dd      3
     search_5:   dd      5
-    search_7:   dd      7
+    search_7:   dd      4
