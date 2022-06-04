8db850abd71058690769a80eb230542e35c31569
Nero Li <1109680304lym@gmail.com>
2022-06-03 21:41:47 -0700

    final lib check

--- final/main.asm
+++ final/main.asm
@@ -26,10 +26,8 @@ _start:
     call    print_string
     pop     edi             ; pop arg1
 
-exit:  
-    mov     ebx, 0      ; return 0 status on exit - 'No Errors'
-    mov     eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
-    int     80h
+    push    dword 0
+    call    exit
 
 section     .bss
     dest:       resb    20      
